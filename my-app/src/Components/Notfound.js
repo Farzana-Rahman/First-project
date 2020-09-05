5992d5f3967278feab3833b82e3ff05bc60818c8
Farzana-Rahman <68366291+Farzana-Rahman@users.noreply.github.com>
2020-09-06 00:22:51 +0200

    some changes on navbar

---
+++ my-app/src/Components/Notfound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Notfound extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Error 404.That's an error.</h1>
+                <p>The requested URL was not found on this server</p>
+            </div>
+        );
+    }
+}
+
+export default Notfound;
--- my-app/src/Components/Routing.js
+++ my-app/src/Components/Routing.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route ,Switch } from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
 import Analytics from './Analytics'
 import Details from './Details';
 import Prevention from './Prevention'
+import Notfound from './Notfound';
 
 class Routing extends Component {
     render() {
 
         return (
             <div>
+                <Switch>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/analytics" component={Analytics}></Route>
                 <Route path="/details" component={Details}></Route>
                 <Route path="/prevention" component={Prevention}></Route>
                 <Route path="/about" component={About}></Route>
-                
+                <Route component={Notfound}></Route>
+                </Switch>
+               
 
             </div>
         );

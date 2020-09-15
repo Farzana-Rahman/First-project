3abfab38c985885a9840a4dc7253982de9450f0f
Farzana-Rahman <68366291+Farzana-Rahman@users.noreply.github.com>
2020-09-15 22:43:45 +0200

    updated few components

--- my-app/src/Components/Details.js
+++ my-app/src/Components/Details.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react';
 class Details extends Component {
     render() {
         return (
-            <div>
-                <h1>This is Details page</h1>
-            </div>
+            <div className="container">
+                COVID-19 affects different people in different ways. Most infected people will develop mild to moderate illness and recover without hospitalization.
+
+                <p>Most common symptoms:</p>
+
+                <p> fever</p>
+
+                <p>  dry cough</p>
+
+                <p> tiredness </p>
+
+                <p>  Less common symptoms: </p>
+
+                <p> aches and pains </p>
+
+                <p> sore throat </p>
+
+                <p> diarrhoea </p>
+
+                <p> conjunctivitis </p>
+                
+                <p>headache </p>
+
+                <p> loss of taste or smell </p>
+
+                <p>  a rash on skin, or discolouration of fingers or toes </p>
+
+                </div>
         );
     }
 }

74a284fc30cf74a9b39c7ba7c7af6daf4035b931
Farzana-Rahman <68366291+Farzana-Rahman@users.noreply.github.com>
2020-09-20 20:53:54 +0200

    updated about page

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <div>
-                <h1>This is About page</h1>
+            <>
+            <div className="container" style={{marginTop:"55px"}}>
+                <h3>About COVID-19 Tracker </h3>
+                <h5>Nowdays COVID-19 is a burning issue.This COVID-19 Tracker will help us to keep track of the latest statistics about COVID-19.It will help us to know global statistics as well as countrywise statistics and all the details about COVID-19.All the contents are filtered and same as Worldmeters.It ensures that sources are reliable  as all the data from WHO,Worldmeter & Wikipedia </h5>
+                <h5>This app also aims to gather all deatils about corona virus and helps people to know the latest news.And as a conscious citizen everyone should know the deatils about the fatal disease to stay safe and protect others from COVID-19. </h5>
+
+            
+
             </div>
+            <div className="footer-sec">
+            <h4>Developed By : Farzana Rahman</h4>
+
+            </div>
+            </>
         );
     }
 }
